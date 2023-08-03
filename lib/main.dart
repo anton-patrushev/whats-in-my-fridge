b88ffb8c452902725b18b6e8ec8ffdf879b24d19
anton-patrushev <anton11patrushev@gmail.com>
2023-08-03 16:15:10 +0200

    feat: add default automaric firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'package:whats_in_my_fridge/firebase_options.dart';
 import 'package:whats_in_my_fridge/src/app/main_app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const AppWithGlobalContexts(child: MainApp()));
 }
