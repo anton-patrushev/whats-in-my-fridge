12635b077461a77702ba0e213f530ab74fab85e5
anton-patrushev <anton11patrushev@gmail.com>
2023-10-10 00:30:39 +0200

    feat: install firebase-cloud-firestore

--- lib/configs/firebase/init_firebase.dart
+++ lib/configs/firebase/init_firebase.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:whats_in_my_fridge/configs/environment/environment_config.dart';
 
 import 'package:whats_in_my_fridge/configs/firebase/firebase_options.dart';
 
@@ -6,4 +8,8 @@ Future<void> initFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (Enviroment.config.isDebug) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
 }
